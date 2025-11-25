b35e9c4ab4dcd3e9ed296bb2d586f9e33e750983
Cucorn <zeromassmedia@gmail.com>
2025-11-25 09:32:50 -0500

    Update version.php

--- report_fileusechecker/version.php
+++ report_fileusechecker/version.php
@@ -1,1 +1,9 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
 
+$plugin->component = 'report_fileusechecker';
+$plugin->version = 2025022500;
+$plugin->requires = 2024100800; // Moodle 5.1
+$plugin->maturity = MATURITY_BETA;
+$plugin->release = '1.0.0';
+$plugin->cron = 0;
