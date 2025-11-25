24593faf262918b5a7f972abb3b4fe248ac74263
Cucorn <zeromassmedia@gmail.com>
2025-11-25 11:18:54 -0500

    Update access.php

--- report_fileusechecker/db/access.php
+++ report_fileusechecker/db/access.php
@@ -1,24 +1,39 @@
 <?php
+/**
+ * Capabilities file for the File Use Checker report plugin.
+ *
+ * @package    report_fileusechecker
+ * @copyright  2025 Sean Rugge
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 defined('MOODLE_INTERNAL') || die();
 
 $capabilities = array(
+
     'report/fileusechecker:view' => array(
-        'captype' => 'view',
+        // This capability allows users to view the report.
+        'captype' => 'read',
         'contextlevel' => CONTEXT_COURSE,
         'archetypes' => array(
-            'teacher' => CAP_ALLOW,
-            'editingteacher' => CAP_ALLOW,
-            'coursecreator' => CAP_ALLOW,
             'manager' => CAP_ALLOW,
-        )
+            'coursecreator' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW,
+        ),
+        'clonepermissionsfrom' => 'moodle/site:viewreports'
     ),
+
     'report/fileusechecker:delete' => array(
+        // This capability allows users to delete files listed in the report.
         'captype' => 'write',
         'contextlevel' => CONTEXT_COURSE,
         'archetypes' => array(
-            'editingteacher' => CAP_ALLOW,
-            'coursecreator' => CAP_ALLOW,
             'manager' => CAP_ALLOW,
-        )
+            'coursecreator' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+        ),
+        // Teachers typically should not be able to delete course files without editing rights.
+        'clonepermissionsfrom' => 'moodle/course:managefiles'
     ),
 );
